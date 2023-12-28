a576a654a85a150f34873c3da475808ce8d4cde2
Nitin Rajan <nitin.rajan15@gmail.com>
2023-12-28 14:27:46 +0530

    First Phase completed

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -5,8 +5,16 @@ class MobileMenu {
         this.menuHeader = document.querySelector('.site-header')
         this.menuElement = document.querySelectorAll('.menu-element')
 
+        this.innermenuIcon = document.querySelector('.inner-header__menu-icon')
+        this.innermenuContent = document.querySelector('.inner-header__nav')
+        this.innermenuHeader = document.querySelector('.inner-header')
+
         if(this.menuIcon || this.menuContent || this.menuHeader) {
             this.events()
+        }
+
+        if(this.innermenuIcon || this.innermenuContent || this.innermenuHeader) {
+            this.innerevents()
         }
     }
 
@@ -14,11 +22,21 @@ class MobileMenu {
         this.menuIcon.addEventListener("click", () => this.toggleTheMenu())
     }
 
+    innerevents() {
+        this.innermenuIcon.addEventListener("click", () => this.innertoggleTheMenu())
+    }
+
     toggleTheMenu() {
         this.menuContent.classList.toggle('site-header__nav--is-visible')
         this.menuHeader.classList.toggle('site-header--is-visible')
         this.menuIcon.classList.toggle('site-header__menu-icon--close-x')
-    }    
+    }
+    
+    innertoggleTheMenu() {
+        this.innermenuContent.classList.toggle('inner-header__nav--is-visible')
+        this.innermenuHeader.classList.toggle('inner-header--is-visible')
+        this.innermenuIcon.classList.toggle('inner-header__menu-icon--close-x')
+    }
 }
 
 export default MobileMenu;
--- app/assets/scripts/modules/Scroller.js
+++ app/assets/scripts/modules/Scroller.js
@@ -1,26 +1,32 @@
-const scrollers = document.querySelectorAll(".scroller");
+class Scroller {
+  constructor() {
+      this.scrollers = document.querySelectorAll(".scroller");
 
-// If a user hasn't opted in for recuded motion, then we add the animation
-if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
-  addAnimation();
-}
+      // If a user hasn't opted in for recuded motion, then we add the animation
+      if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+        this.addAnimation();
+      }
+  }
 
-function addAnimation() {
-  scrollers.forEach((scroller) => {
-    // add data-animated="true" to every `.scroller` on the page
-    scroller.setAttribute("data-animated", true);
-
-    // Make an array from the elements within `.scroller-inner`
-    const scrollerInner = scroller.querySelector(".scroller__inner");
-    const scrollerContent = Array.from(scrollerInner.children);
-
-    // For each item in the array, clone it
-    // add aria-hidden to it
-    // add it into the `.scroller-inner`
-    scrollerContent.forEach((item) => {
-      const duplicatedItem = item.cloneNode(true);
-      duplicatedItem.setAttribute("aria-hidden", true);
-      scrollerInner.appendChild(duplicatedItem);
+  addAnimation() {
+    this.scrollers.forEach((scroller) => {
+      // add data-animated="true" to every `.scroller` on the page
+      scroller.setAttribute("data-animated", true);
+  
+      // Make an array from the elements within `.scroller-inner`
+      const scrollerInner = scroller.querySelector(".scroller__inner");
+      const scrollerContent = Array.from(scrollerInner.children);
+  
+      // For each item in the array, clone it
+      // add aria-hidden to it
+      // add it into the `.scroller-inner`
+      scrollerContent.forEach((item) => {
+        const duplicatedItem = item.cloneNode(true);
+        duplicatedItem.setAttribute("aria-hidden", true);
+        scrollerInner.appendChild(duplicatedItem);
+      });
     });
-  });
+  }
 }
+
+export default Scroller;
